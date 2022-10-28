bef84cdd0b795628d64303bf3f44383bf007686c
Drew Anello <drew.anello@gmail.com>
2022-10-28 01:07:56 -0400

    first commit

---
+++ src/components/WeatherInformation.vue
@@ -0,0 +1,15 @@
+<template>
+    <div class="container">
+        <div class="inner-container">
+            
+        </div>
+    </div>
+</template>
+<script>
+export default {
+    
+}
+</script>
+<style lang="">
+    
+</style>
